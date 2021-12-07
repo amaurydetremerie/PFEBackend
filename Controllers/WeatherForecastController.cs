d864da65c5f61c04234907ea597c87c7ca4b93bf
Amaury De Tremerie <amaury.detremerie@student.vinci.be>
2021-12-07 22:34:11 +0100

    Route avec et sans permission

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -7,9 +7,12 @@ namespace PFEBackend.Controllers
     [Authorize]
     [ApiController]
     [Route("[controller]")]
-    [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
     public class WeatherForecastController : ControllerBase
     {
+        static readonly string[] scopeRequiredByApi = new string[] { "access_as_user" };
+
+        static readonly string[] emptyScope = new string[] {};
+
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -25,6 +28,20 @@ namespace PFEBackend.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByApi);
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet(Name = "GetWeatherForecastFree")]
+        public IEnumerable<WeatherForecast> GetFree()
+        {
+            HttpContext.VerifyUserHasAnyAcceptedScope(emptyScope);
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
